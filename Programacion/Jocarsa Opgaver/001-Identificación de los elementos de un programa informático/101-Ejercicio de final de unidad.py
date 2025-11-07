95606a94a2459ef418eb5e0de64d431239a3bf62
Oscar Sorensen <oscarsorensendk@gmail.com>
2025-11-07 10:03:40 +0100

    gg

---
+++ Programacion/Jocarsa Opgaver/001-Identificación de los elementos de un programa informático/101-Ejercicio de final de unidad.py
@@ -0,0 +1,21 @@
+
+"""
+Descripción: Este programa está diseñado para registrar y mostrar información básica sobre un producto, aplicando los conceptos de la Unidad 1.
+v0.1 Oscar Sorensen
+Este programa es una examen para el 15 de octubre de 2025.
+Nombre del programo: factura_con_iva_descuento.py
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
